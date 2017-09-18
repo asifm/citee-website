acfb40e271e901d35fcceb72cc44f504be651ef2
asif <asif.mehedi@live.com>
2017-09-18 13:09:26 -0400

    Add default layout and index file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,44 @@
-<template>
-  <div>
-    <nuxt/>
-  </div>
+<template lang="pug">
+  v-app
+    //- top bar
+    v-toolbar.elevation-3
+      img(src="~/assets/img/darden_rgb-02.png" height="60px")
+      v-spacer
+      img(src="~/assets/img/citee-logo-02.svg" height="50px")
+    //-page content
+    nuxt
+    //- Footer
+    v-container(fluid).secondary.footer-about.pa-5.elevation-3
+      v-layout(row wrap).grey--text.text--lighten-1
+        //- v-flex(sm5 offset-sm1 md4 offset-md1)
+        v-flex(sm5 md4 offset-md2)
+          h5.white--text ABOUT DARDEN SCHOOL OF BUSINESS
+          p The University of Virginia Darden School of Business delivers the world’s best business education experience to prepare entrepreneurial, global and responsible leaders through its MBA, Ph.D. and Executive Education programs. Darden’s top-ranked faculty is renowned for teaching excellence and advances practical business knowledge through research. Darden was established in 1955 at the University of Virginia, a top public university founded by Thomas Jefferson in 1819 in Charlottesville, Virginia.
+          v-btn.btn--outline.ml-0.primary
+            a(href="http://www.darden.virginia.edu/") Learn More
+          br
+          v-icon(dark).pr-1 mdi-twitter
+          span 
+            a(href="https://twitter.com/@DardenMBA") @DardenMBA
+          br
+          v-icon(dark).pr-1 mdi-email
+          span 
+            a(href="mailto:Darden@virginia.edu") Darden@virginia.edu
+          br
+        
+        //- v-flex(sm5 offset-sm1 md4 offset-md1)
+        v-flex(sm5 md4 offset-sm1)
+          h5.white--text ABOUT BATTEN INSTITUTE 
+          p  The Batten Institute for Entrepreneurship and Innovation at the Darden School of Business improves the world by creating knowledge about the transformative power of entrepreneurship and innovation and by cultivating principled, entrepreneurial leaders. The Institute's research initiative advances knowledge that addresses real-world challenges and shapes Darden's curriculum, and its educational offerings are ranked top 10 in the world. The Batten Institute was established by the vision and philanthropy of UVA alumnus Frank Batten Sr.
+          v-btn.btn--outline.ml-0.primary
+            a(href="http://www.darden.virginia.edu/") Learn More
+          br
+          v-icon(dark).pr-1 mdi-twitter
+          span
+            a(href="@BattenInstitute") @BattenInstitute 
+          br
+          v-icon(dark).pr-1 mdi-email
+          span 
+            a(href="mailto:Batten@darden.virginia.edu") Batten@darden.virginia.edu
+      p.white--text.pt-5.text-sm-center © 2017 BY THE RECTOR AND VISITORS OF THE UNIVERSITY OF VIRGINIA
 </template>
-
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,78 @@
 <template lang="pug">
+  v-container(fluid)#landing-top.text-xs-center
+    v-layout.justify-center
+      v-flex(xs12 sm8 md5 lg4).pa-0
+        transition(name="fade" appear)
+          div(v-if="show")
+            img(src="~/assets/img/citee-logo-white.svg").ma-3.dropshadow.opacity-6
+        h4.pa-3.secondary.primary--text.elevation-12.opacity-9 Cities Innovating Tomorrow's Entrepreneurial Ecosystems
+    v-layout(row wrap).pa-0.justify-center
+      v-flex(xs12 sm5 md3 lg2).pa-3
+        v-card.py-3.elevation-24.grey.lighten-3.opacity-7
+          v-icon.display-4.blue-grey--text mdi-chart-bar
+          h6 Visualizations
+      v-flex(xs12 sm5 md3 lg2).pa-3
+        v-card.py-3.elevation-24.grey.lighten-3.opacity-7
+          v-icon.display-4.blue-grey--text mdi-map
+          h6 Case Studies
+      v-flex(xs12 sm5 md3 lg2).pa-3
+        v-card.py-3.elevation-24.grey.lighten-3.opacity-7
+          v-icon.display-4.blue-grey--text mdi-database
+          h6 Resources
+      v-flex(xs12 sm5 md3 lg2).pa-3
+        v-card.py-3.elevation-24.grey.lighten-3.opacity-7
+          v-icon.display-4.blue-grey--text mdi-book-open
+          h6 Blog
+    
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 5s ease-out;
+}
+
+fade-enter-to {
+  opacity: 1
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+
+#landing-top {
+  background-size: cover;
+  background-position: 50% 50%;
+  background-attachment: scroll;
+  background-image: url('~/assets/img/landing-background.jpg');
+  img {
+    width: 300px;
+  } // text under logo
+  h4 {
+    font-weight: bold; // text-transform: uppercase;
+    letter-spacing: 0.1em;
+  } // cards
+  h6 {
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  .dropshadow {
+    -webkit-filter: drop-shadow(0 7px 8px rgba(0, 0, 0, 0.1));
+    filter: drop-shadow( 0 17px 8px rgba(0, 0, 0, 1));
+    /* Same syntax as box-shadow */
+  }
+}
+</style>
