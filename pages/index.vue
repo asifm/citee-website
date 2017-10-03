cdae119ff36b1fb3e9da58d24135a2645161ad02
asif <asif.mehedi@live.com>
2017-10-03 18:57:49 -0400

    Sepia

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         transition(name="fade" appear)
           div(v-if="show")
             img(src="~/assets/img/citee-logo-white.svg").ma-3.dropshadow.opacity-6
-            h4.pa-3.secondary.primary--text.elevation-12.opacity-9 Cities Innovating Tomorrow's Entrepreneurial Ecosystems
+            h4.pa-3.primary--text.elevation-12#text-bg Cities Innovating Tomorrow's Entrepreneurial Ecosystems
     transition(name="fade" appear)
       v-layout(row wrap v-if="show").pa-0.justify-center
         v-flex(xs12 sm5 md3 lg2).pa-3
@@ -61,7 +61,11 @@ fade-enter-to {
   background-size: cover;
   background-position: 50% 50%;
   background-attachment: scroll;
-  background-image: url('~/assets/img/landing-background.jpg');
+  background-image: url('~/assets/img/landing-background.jpg'); // filter: grayscale(1);
+  // filter: blur(20x);
+  // filter: brightness(.8);
+  // filter: contrast(90%);
+  filter: sepia(.4);
   img {
     width: 300px;
   } // text under logo
@@ -77,6 +81,9 @@ fade-enter-to {
     -webkit-filter: drop-shadow(0 7px 8px rgba(0, 0, 0, 0.1));
     filter: drop-shadow( 0 17px 8px rgba(0, 0, 0, 1));
     /* Same syntax as box-shadow */
+  }
+  #text-bg {
+    background-color: rgba(0, 18, 42, 0.6);
   }
 }
 </style>
