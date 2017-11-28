fce416f7206a6c2390aacd1b247847262c00c9b5
asif <asif.mehedi@live.com>
2017-11-28 02:31:13 -0500

    testpage for map

---
+++ pages/testleaflet.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  no-ssr
+    //- Available props: zoomval, centerval, markerval
+    DrawMap(:zoomval="zoom")
+
+</template>
+
+<script>
+import DrawMap from '../components/DrawMap.vue';
+
+export default {
+  components: { DrawMap },
+  data() {
+    return {
+      zoom: 20,
+      center: [47.41322, -1.219482],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: [47.41322, -1.219482],
+    };
+  },
+};
+</script>
+
+<style>
+#map {
+  height: 1100px;
+}
+</style>
