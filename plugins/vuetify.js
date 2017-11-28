1b06e82b69aab7773fb5baac7e62ace9de8c1974
asif <asif.mehedi@live.com>
2017-11-28 02:33:04 -0500

    Move vuetify theme to plugins

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#E57200',
+    secondary: '#232D4B',
+    accent: '#F2CD00',
+  },
+});
