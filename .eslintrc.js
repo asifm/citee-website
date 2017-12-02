63bc7b624bcd2e1b9b29f3a33a3663da1d6ffb1c
asif <asif.mehedi@live.com>
2017-12-02 03:51:50 -0500

    Update eslint, babel configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,16 @@ module.exports = {
   rules: {
     'no-console': 0,
     'no-param-reassign': ['error', { props: false }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
   },
   globals: {},
 };
