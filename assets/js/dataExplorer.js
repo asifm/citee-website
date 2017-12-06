7e8828abaf2ad317125af106f7de12cfd6646165
asif <asif.mehedi@live.com>
2017-12-05 19:21:51 -0500

    Scatter plot working with four variables

---
+++ assets/js/dataExplorer.js
@@ -0,0 +1,444 @@
+import * as d3 from 'd3';
+import * as axios from 'axios';
+
+/**
+ * Converts strings to numbers where appropriate
+ *
+ * @param {object} d row of data parsed from csv
+ * @returns {object} a row of data with numbers as numbers, not strings
+ */
+function processData(d) {
+  return {
+    business_starts_2014_per10K: +d.business_starts_2014_per10K,
+    business_exits_2014_per10K: +d.business_exits_2014_per10K,
+    cagr01to15: +d.cagr01to15,
+    cagr10to15: +d.cagr10to15,
+    cbsa15: d.cbsa15,
+    cbsaname15: d.cbsaname15,
+    census_region: d.census_region,
+    employed: +d.employed_count,
+    fips_state: d.fips_state,
+    fortune1000_count: +d.fortune1000_count,
+    gini: +d.gini,
+    gini_moe: +d.gini_moe,
+    gmp_billion_usd: +d.gmp_billion_usd,
+    gmp_per_cap_usd: +d.gmp_per_cap_usd,
+    inc5000_count: +d.inc5000_count,
+    jobs_created_2014_per1K: +d.jobs_created_2014_per1K,
+    labor_force_count: +d.labor_force_count,
+    lat: +d.lat,
+    long: +d.long,
+    patents_count: +d.patents_count,
+    patents_count_top_class: +d.patents_count_top_class,
+    patents_per10K: +d.patents_per10K,
+    population14: +d.pop14,
+    restaurants_count: +d.restaurants_count,
+    restaurants_per10K: +d.restaurants_per10K,
+    unicorns_count: +d.unicorns_count,
+    unemployment_percent: +d.unemployment_percent / 100,
+    unemployed_count: +d.unemployed_count,
+    unicorns_per1M: +d.unicorns_per1M,
+    inc5000_per1M: +d.inc5000_per1M,
+    fortune1000_per1M: +d.fortune1000_per1M,
+    top_patent_class_percent: +d.top_patent_class_percent,
+    women_in_workforce_percent: +d.women_in_workforce_percent,
+    top_patent_class: d.top_patent_class,
+    state_abbr: d.state_abbr,
+    state_name: d.state_name,
+  };
+}
+
+/**
+ * First parses CSV and then processes rows (converts to correct types)
+ *
+ * @param {any} filepath Path to the csv file
+ * @returns {object} Data with correct types
+ */
+async function getParsedData(filepath) {
+  const response = await axios.get(filepath);
+  const parsedData = d3.csvParse(response.data);
+  return parsedData;
+}
+
+function drawSvgScatter(container, width, height, margin) {
+  const svg = d3
+    .select(container)
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .style('filter', 'url(#dropshadow)')
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  return svg;
+}
+
+// TODO: add linear scale
+// TODO: Add color scale
+// TODO: Add map circle radius scale
+function createScales(width, height, data, xVar, yVar, radiusVar, colorVar) {
+  // TODO: negative values have been filtered out. Otherwise logscales won't work. Find a solution.
+  const radiusScale = d3.scaleSqrt().range([3, 20]);
+  const xScale = d3.scaleLog().range([25, width - 25]);
+  const yScale = d3.scaleLog().range([height - 25, 25]);
+  const colorScale = d3.scaleLog().range([d3.rgb('#FFD500'), d3.rgb('#007AFF')]);
+  // const colorScale = d3.scaleLog(d3.interpolatePlasma);
+
+  d3.scaleLinear().range(['red', 'blue']);
+  // Scale based on the range of the data
+  const minX = d3.min(data, d => d[xVar] || Infinity);
+  const minY = d3.min(data, d => d[yVar] || Infinity);
+  const minRadius = d3.min(data, d => d[radiusVar] || Infinity);
+  const minColor = d3.min(data, d => d[colorVar] || Infinity);
+
+  const maxX = d3.max(data, d => d[xVar]);
+  const maxY = d3.max(data, d => d[yVar]);
+  const maxRadius = d3.max(data, d => d[radiusVar]);
+  const maxColor = d3.max(data, d => d[colorVar]);
+
+  xScale.domain([minX, maxX]);
+  yScale.domain([minY, maxY]);
+  radiusScale.domain([minRadius, maxRadius]);
+  colorScale.domain([minColor, maxColor]);
+
+  return [xScale, yScale, radiusScale, colorScale];
+}
+
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    Axis, ticks, texts
+    Draw before drawing circles so circles are on top; makes hover experience better
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+// To be
+function setTicks(xScale, yScale, xAxis, yAxis) {
+  // for sideeffect; no return
+  // Set ticks format based on largest value (use current domains to get the value)
+  if (xScale.domain()[1] < 100) {
+    xAxis.ticks(5, '0.2f');
+  } else {
+    xAxis.ticks(5, '0.2s');
+  }
+
+  if (yScale.domain()[1] < 100) {
+    yAxis.ticks(5, '0.2f');
+  } else {
+    yAxis.ticks(5, '0.2s');
+  }
+}
+
+function drawAxes(svg, width, height, margin, xVarText, yVarText, xScale, yScale) {
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+  setTicks(xScale, yScale, xAxis, yAxis);
+  // Add the x Axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0,${height})`)
+    .call(xAxis)
+    .style('font-family', 'Open Sans')
+    .style('font-size', '0.8em')
+    .style('stroke-opacity', 1)
+    .style('stroke-width', '3.5px');
+  // .attr('class', 'axis axis--x');
+
+  // Add the y Axis
+  svg
+    .append('g')
+    .call(yAxis)
+    .style('font-family', 'Open Sans')
+    .style('font-size', '0.8em')
+    .style('stroke-opacity', 1)
+    .style('stroke-width', '3.5px');
+  // .attr('class', 'axis axis--y');
+
+  // text label for the x axis
+  svg
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', height + margin.bottom)
+    .attr('dy', '-55px')
+    .style('font-size', '1.3em')
+    .style('text-anchor', 'middle')
+    .style('font-family', 'Open Sans')
+    .text(xVarText);
+
+  // text label for the y axis
+  svg
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', -margin.left)
+    .attr('x', -height / 2)
+    .attr('dy', '55px')
+    .style('font-size', '1.3em')
+    .style('text-anchor', 'middle')
+    .style('font-family', 'Open Sans')
+    .text(yVarText);
+
+  // add the X gridlines
+  svg
+    .append('g')
+    .attr('class', 'grid x')
+    .style('stroke-opacity', '0.2')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3
+      .axisBottom(xScale)
+      .tickSize(-height)
+      .tickFormat(''));
+
+  // add the Y gridlines
+  svg
+    .append('g')
+    .attr('class', 'grid y')
+    .style('stroke-opacity', '0.1')
+    .call(d3
+      .axisLeft(yScale)
+      .tickSize(-width)
+      .tickFormat(''));
+
+  return [xAxis, yAxis];
+}
+
+function drawCircles(
+  svg,
+  data,
+  xVar,
+  yVar,
+  radiusVar,
+  colorVar,
+  mapRadiusVar,
+  xScale,
+  yScale,
+  radiusScale,
+  colorScale,
+  mapRadiusScale,
+  xVarText,
+  yVarText,
+  radiusVarText,
+  colorVarText,
+  mapRadiusVarText,
+  xFormat,
+  yFormat,
+  radiusFormat,
+  colorFormat,
+  mapRadiusFormat
+) {
+  const circles = svg
+    .selectAll('.dot')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('cx', d => xScale(d[xVar]))
+    .attr('cy', d => yScale(d[yVar]))
+    .attr('r', d => radiusScale(d[radiusVar]))
+    .attr('fill', d => colorScale(d[colorVar]))
+    .attr('opacity', '0.4')
+    .attr('stroke-width', '0.5')
+    .attr('stroke', 'black');
+  // .append('g');
+
+  const tooltip = d3
+    .select('body')
+    .append('div')
+    .attr('class', 'card card--raised')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
+    .style('font-size', '14px')
+    .style('padding', '10px')
+    .style('line-height', '18px')
+    .style('border-radius', '3px')
+    .attr('class', 'tooltip box viz-shadow');
+
+  circles.on('mouseover', (d) => {
+    tooltip
+      .html('')
+      .style('visibility', 'visible')
+      .html(() => `<h4>${d.cbsaname15}</h4>
+                  <br><strong>${d3.format(xFormat)(d[xVar])}</strong> ${xVarText}
+                  <br><strong>${d3.format(yFormat)(d[yVar])}</strong> ${yVarText}
+                  <br><strong>${d3.format(radiusFormat)(d[radiusVar])}</strong> ${radiusVarText}
+                  <br><strong>${d3.format(colorFormat)(d[colorVar])}</strong> ${colorVarText}
+                  <br><strong>${d3.format(mapRadiusFormat)(d[mapRadiusVar])}</strong> ${
+  mapRadiusVarText
+}`);
+  });
+
+  circles.on('mousemove', () =>
+    tooltip.style('top', `${d3.event.pageY - 52}px`).style('left', `${d3.event.pageX + 18}px`));
+
+  circles.on('mouseout', () => tooltip.style('visibility', 'hidden'));
+}
+
+function drawMap() {}
+function drawMapCircles() {}
+function createTooltips() {}
+function createDetailData() {}
+
+/*
+name: name of the varialbe
+text: text to display to user
+type: number or category, etc. (to use to convert to correct tyep when parsing)
+format: format to show to user (use d3.format to do this)
+include: whether to include in the scatter or not
+default: default var for x axis, y axis, etc.
+*/
+const metaDataArr = [
+  {
+    name: 'cagr01to15',
+    text: 'Annual Growth Rate 2001–15',
+    type: 'number',
+    format: ',.1%',
+    include: true,
+  },
+  {
+    name: 'cagr10to15',
+    text: 'Annual Growth Rate 2010–15',
+    type: 'number',
+    format: ',.1%',
+    include: true,
+    default: 'mapRadius',
+  },
+  {
+    name: 'business_starts_2014_per10K',
+    text: 'Business starts in 2014 per 10K people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'gmp_billion_usd',
+    text: 'Gross Metropolitan Product (Bn USD)',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'gmp_per_cap_usd',
+    text: 'Gross Metropolitan Product per Capita (USD)',
+    type: 'number',
+    format: ',.0f',
+    include: false,
+  },
+  {
+    name: 'gini',
+    text: 'Inequality (Gini index)',
+    type: 'number',
+    format: '.2f',
+    include: true,
+  },
+  {
+    name: 'jobs_created_2014_per1K',
+    text: 'Jobs created in 2014 per 1K people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+    default: 'color',
+  },
+  {
+    name: 'fortune1000_count',
+    text: 'Fortune 1000 Companies',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'fortune1000_per1M',
+    text: 'Fortune 1000 per 1M people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'inc5000_count',
+    text: 'Inc 5000 Companies',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'inc5000_per1M',
+    text: 'Inc 5000 per 1M people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'patents_count',
+    text: 'Total patents',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'patents_per10K',
+    text: 'Patents per 10K people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+    default: 'yAxis',
+  },
+  {
+    name: 'restaurants_per10K',
+    text: 'Restaurants per 10K people',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+    default: 'xAxis',
+  },
+  {
+    name: 'unicorns_count',
+    text: 'Unicorns',
+    type: 'number',
+    format: ',.0f',
+    include: false,
+  },
+  {
+    name: 'unicorns_per1M',
+    text: 'Unicorns per 1M people',
+    type: 'number',
+    format: ',.0f',
+    include: false,
+  },
+  {
+    name: 'population14',
+    text: 'Population',
+    type: 'number',
+    format: ',.0f',
+    include: true,
+  },
+  {
+    name: 'unemployed_count',
+    text: 'Unemployed population',
+    type: 'number',
+    format: ',.0f',
+    include: false,
+  },
+  {
+    name: 'unemployment_percent',
+    text: 'Unemployment Rate (Feb 2017)',
+    type: 'number',
+    format: ',.1%',
+    include: true,
+    default: 'radius',
+  },
+  {
+    name: 'women_in_workforce_percent',
+    text: 'Women in Workforce (%)',
+    type: 'number',
+    format: ',.0%',
+    include: false,
+  },
+];
+export {
+  getParsedData,
+  processData,
+  metaDataArr,
+  drawSvgScatter,
+  createScales,
+  drawAxes,
+  drawCircles,
+  drawMap,
+  drawMapCircles,
+  createTooltips,
+  createDetailData,
+};
--- pages/tool/DataExplorer.vue
+++ pages/tool/DataExplorer.vue
@@ -5,30 +5,100 @@ v-container(fluid)
       h4 Metro Data Explorer
 
   //- top row for controls; alternatively, controls on the side of the scatter plot in a single column
-  v-layout
-    v-flex(lg3)
-      //- select x and y variables
-      v-card.pa-4.grey.lighten-4.card--raised
-        v-select(label="Variable for x axis")
-        v-select(label="Variable for y axis")
-    v-flex(lg3)
-      v-card.pa-4.card--raised
-        v-select(label="Variable for circle size")
-        v-select(label="Variable for circle color")
-    v-flex(lg3)
-      v-card.pa-4.grey.lighten-4.card--raised
-        v-select(label="Variable for map circle size")
-        //- use log scale
-        v-slider(label="Select metro population range")
-    v-flex(lg3)
-      v-card.pa-4.card--raised
-        v-select(label="Select metro")
-        v-select(label="Select state")
+  v-layout(row wrap).pa-3
+    v-flex(lg7 md12).pa-3
+      v-card.pa-2.card--raised
+        svg#svg-scatter
+          filter#dropshadow
+            feGaussianBlur(in="SourceAlpha" stdDeviation="10")
+            feOffset(dx="0", dy="0")
+            feMerge
+              feMergeNode
+              feMergeNode(in="SourceGraphic")
+   
+    v-flex(lg3 md6).pa-3
+      v-layout(row wrap)
+        v-flex(lg12)
+          //- select x and y variables
+          v-card.pa-4.grey.lighten-4.card--raised
+            v-select(
+              label="Variable for x axis"
+              autocomplete
+              dense
+              :items="varsArr"
+              v-model="xVar"
+              item-value="name"
+              item-text="text"
+              @select="renderScatter()"
+              )
+            v-select(
+              label="Variable for y axis"
+              autocomplete
+              dense
+              :items="varsArr"
+              v-model="yVar"
+              item-value="name"
+              item-text="text"
+              @select="renderScatter()"
+              )
+
+            v-select(
+              label="Variable for circle size"
+              autocomplete
+              dense
+              :items="varsArr"
+              v-model="radiusVar"
+              item-value="name"
+              item-text="text"
+              @select="renderScatter()"
+              )
+            v-select(
+              label="Variable for circle color"
+              autocomplete
+              dense
+              :items="varsArr"
+              v-model="colorVar"
+              item-value="name"
+              item-text="text"
+              @select="renderScatter()"
+              )
+
+            v-select(
+              label="Variable for map circle size"
+              autocomplete
+              dense
+              :items="varsArr"
+              v-model="mapRadiusVar"
+              item-value="name"
+              item-text="text"
+              @select="renderScatter()"
+              )
+              
+        v-flex(lg12)
+          v-card.pa-4.card--raised
+            //- use log scale
+            v-slider(label="Metro population range")
+
+            v-select(
+              label="Select metro"
+              autocomplete
+              clearable
+              chips
+              dense
+              :items="metrosArr"
+              v-model="selectedMetrosArr"
+              )
+            v-select(
+              label="Select state"
+              autocomplete
+              clearable
+              chips
+              dense
+              :items="statesArr"
+              v-model="selectedStatesArr"
+              )
   
-  //- scatter plot; main visual element of the map; center and big
-  v-layout
-    v-flex(lg8)
-  
+
   v-layout
     //- map
     v-flex(lg6)
@@ -39,4 +109,204 @@ v-container(fluid)
         v-flex(lg6)
         //- bottom 5
         v-flex(lg6)
+
 </template>
+
+<script>
+// import * as d3 from 'd3';
+// import * as axios from 'axios';
+import {
+  getParsedData,
+  processData,
+  metaDataArr,
+  drawSvgScatter,
+  createScales,
+  drawAxes,
+  drawCircles,
+  drawMap,
+  drawMapCircles,
+  createTooltips,
+  createDetailData,
+} from '../../assets/js/dataExplorer';
+
+const varsArr = metaDataArr.filter(elem => elem.include);
+
+export default {
+  data() {
+    return {
+      // TODO: Combine all similar attributs into an object for
+      // easy passing of data
+      svgScatterContainer: '#svg-scatter',
+      svgScatter: null,
+      width: 800,
+      height: 800,
+      margin: {
+        top: 20,
+        right: 20,
+        bottom: 120,
+        left: 120,
+      },
+      xScaleType: 'xlog',
+      yScaleType: 'ylog',
+      xScale: Function,
+      yScale: Function,
+      radiusScale: Function,
+      colorScale: Function,
+      mapRadiusScale: Function,
+      xAxis: Function,
+      yAxis: Function,
+      filepath: '/data/data_explorer.csv',
+      // pData => parsed and processed data
+      pData: [],
+      xVar: varsArr.filter(elem => elem.default === 'xAxis')[0].name,
+      yVar: varsArr.filter(elem => elem.default === 'yAxis')[0].name,
+      colorVar: varsArr.filter(elem => elem.default === 'color')[0].name,
+      radiusVar: varsArr.filter(elem => elem.default === 'radius')[0].name,
+      mapRadiusVar: varsArr.filter(elem => elem.default === 'mapRadius')[0]
+        .name,
+      xVarText: varsArr.filter(elem => elem.default === 'xAxis')[0].text,
+      yVarText: varsArr.filter(elem => elem.default === 'yAxis')[0].text,
+      colorVarText: varsArr.filter(elem => elem.default === 'color')[0].text,
+      radiusVarText: varsArr.filter(elem => elem.default === 'radius')[0].text,
+      mapRadiusVarText: varsArr.filter(elem => elem.default === 'mapRadius')[0]
+        .text,
+      xFormat: varsArr.filter(elem => elem.default === 'xAxis')[0].format,
+      yFormat: varsArr.filter(elem => elem.default === 'yAxis')[0].format,
+      colorFormat: varsArr.filter(elem => elem.default === 'color')[0].format,
+      radiusFormat: varsArr.filter(elem => elem.default === 'radius')[0].format,
+      mapRadiusFormat: varsArr.filter(elem => elem.default === 'mapRadius')[0]
+        .format,
+      selectedMetrosArr: [],
+      selectedStatesArr: [],
+      varsArr,
+      varsNamesArr: varsArr.map(elem => elem.name),
+      varsTextArr: varsArr.map(elem => elem.text),
+      metrosArr: [],
+      statesArr: [],
+    };
+  },
+  methods: {
+    renderSetup() {
+      // select all metros and states on mount for display
+      // TODO: figure out a way to select all without showing more than 3 or 5;
+      // for now, not allowing multiple selection
+      // this.selectedMetrosArr = this.metrosArr;
+      // this.selectedStatesArr = this.statesArr; // it may not be necessary on mount
+      this.svgScatter = drawSvgScatter(
+        this.svgScatterContainer,
+        this.width,
+        this.height,
+        this.margin
+      );
+
+      createTooltips();
+      createDetailData();
+    },
+
+    renderScatter() {
+      // First remove everything before rendering
+      this.svgScatter.selectAll('*').remove();
+
+      // Keep only data where all values for the selected variables are positive.
+      // Otherwise logscales won't work. Find a solution.
+      this.pData = this.pData.filter(d =>
+          d[this.xVar] > 0 &&
+          d[this.yVar] > 0 &&
+          d[this.radiusVar] > 0 &&
+          d[this.colorVar] > 0 &&
+          d[this.mapRadiusVar] > 0);
+      [
+        this.xScale,
+        this.yScale,
+        this.radiusScale,
+        this.colorScale,
+      ] = createScales(
+        this.width,
+        this.height,
+        this.pData,
+        this.xVar,
+        this.yVar,
+        this.radiusVar,
+        this.colorVar
+      );
+
+      // function drawAxes(svg, width, height, margin, xVar, yVar, xScale, yScale, varsArr) {
+      [this.xAxis, this.yAxis] = drawAxes(
+        this.svgScatter,
+        this.width,
+        this.height,
+        this.margin,
+        this.xVarText,
+        this.yVarText,
+        this.xScale,
+        this.yScale
+      );
+
+      drawCircles(
+        this.svgScatter,
+        this.pData,
+        this.xVar,
+        this.yVar,
+        this.radiusVar,
+        this.colorVar,
+        this.mapRadiusVar,
+        this.xScale,
+        this.yScale,
+        this.radiusScale,
+        this.colorScale,
+        this.mapRadiusScale,
+        this.xVarText,
+        this.yVarText,
+        this.radiusVarText,
+        this.colorVarText,
+        this.mapRadiusVarText,
+        this.xFormat,
+        this.yFormat,
+        this.radiusFormat,
+        this.colorFormat,
+        this.mapRadiusFormat
+      );
+    },
+    renderMap() {
+      drawMap();
+      drawMapCircles();
+    },
+  },
+  watch: {
+    xVar() {
+      this.xVarText = this.varsArr.filter(elem => elem.name === this.xVar)[0].text;
+      this.xFormat = this.varsArr.filter(elem => elem.name === this.xVar)[0].format;
+    },
+    yVar() {
+      this.yVarText = this.varsArr.filter(elem => elem.name === this.yVar)[0].text;
+      this.yFormat = this.varsArr.filter(elem => elem.name === this.yVar)[0].format;
+    },
+    radiusVar() {
+      this.radiusVarText = this.varsArr.filter(elem => elem.name === this.radiusVar)[0].text;
+      this.radiusFormat = this.varsArr.filter(elem => elem.name === this.radiusVar)[0].format;
+    },
+    colorVar() {
+      this.colorVarText = this.varsArr.filter(elem => elem.name === this.colorVar)[0].text;
+      this.colorFormat = this.varsArr.filter(elem => elem.name === this.colorVar)[0].format;
+    },
+    mapRadiusVar() {
+      this.mapRadiusVarText = this.varsArr.filter(elem => elem.name === this.mapRadiusVar)[0].text;
+      this.mapRadiusFormat = this.varsArr.filter(elem => elem.name === this.mapRadiusVar)[0].format;
+    },
+  },
+  mounted() {
+    getParsedData('/data/data_explorer.csv').then((data) => {
+      this.pData = data.map(elem => processData(elem));
+
+      this.metrosArr = this.pData.map(elem => elem.cbsaname15);
+      this.statesArr = this.pData.map(elem => elem.state_name);
+      this.renderSetup();
+      this.renderScatter();
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
