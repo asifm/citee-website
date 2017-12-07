f3a399c322c22a573b8b2a38f39e6eb6b13bd3d2
asif <asif.mehedi@live.com>
2017-12-07 18:39:52 -0500

    New eslint rules plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   env: {
     browser: true,
     node: true,
   },
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'plugin:promise/recommended', 'plugin:vue/recommended'],
   // required to lint *.vue files
-  plugins: ['html'],
+  plugins: ['html', 'promise', 'vue'],
   // add your custom rules here
   rules: {
     'no-console': 0,
