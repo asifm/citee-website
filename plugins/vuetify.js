bb5bec5e35f8a177e6fc8c27ecf205a00801f2c9
asif <asif.mehedi@live.com>
2017-12-07 18:40:22 -0500

    Move theme parameters to vuetify plugins

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,10 +3,16 @@ import Vuetify from 'vuetify';
 
 Vue.use(Vuetify, {
   theme: {
+    // primary: '#E57200',
+    // secondary: '#232D4B',
     primary: '#E57200',
-    secondary: '#232D4B',
-    // Possibly new blue
-    // secondary: '#002F6C',
-    accent: '#F2CD00',
+    secondary: '#002F6C',
+    'text--secondary': '#002F6C',
+    'text--primary': '#E57200',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
   },
 });
