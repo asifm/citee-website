65ca1c7192a7d15594417443e9edf05112b51a20
asif <asif.mehedi@live.com>
2018-02-06 12:29:35 -0500

    Add progressbar and narrative text

--- pages/tools/BulkGeocoder.vue
+++ pages/tools/BulkGeocoder.vue
@@ -1,26 +1,36 @@
 <template lang="pug">
-div.grey.lighten-3
+div.grey.darken-2
     div.text-sm-center.pa-3.elevation-3.page-header
         h1 CITee Geocoder (Beta)
-        h2 Convert Locations to Census Legal and Statistical Entities
-    v-container.pa-5
-        v-layout(row wrap)
-            v-flex(lg4).pa-3
+        h2 Convert US Locations to Census Legal and Statistical Entities
+        p.caption The app is in beta phase, but we encourage you to use it. Send your suggestions, bug-reports, and questions to #[a(href="mehedia@darden.virginia.edu") mehedia@darden.virginia.edu].
+    v-container(fluid)
+        v-layout(row wrap).pa-5
+            v-flex(lg6 xl4).pa-3
                 v-card(height="100%").elevation-5
                     v-card-title.brown.lighten-4.elevation-3
                         h2 <strong>Step 1</strong> Prepare Your Data
                     v-card-text.pa-5
-                        p TODO: Instructions for how to prepare the data before uploading.
-                        p.caption This app has been tested only on the latest versions of Chrome. It may or may not work on other browsers.
-                        p.caption Although the app is in beta phase, you're encouraged to use it. Please report bugs and errors and send your suggestions to mehedia@darden.virginia.edu.
-            v-flex(lg4).pa-3
+                        p An input file can have only one type of geodata. If you need to convert different types of geodata, please organize them in multiple files.
+                        p The geodata types that can be converted are:
+                        h3 Zip Codes
+                        p Structure: one Zip code per line in a plain text file.
+                        h3 Addresses
+                        p Structure: one address per line in a plain text file. Unstructured and incomplete addresses are usually OK. Names of well-known institutions may also work.
+                        h3 Geographic coordinates
+                        p Structure: one longitude-latitude (in that order) pair per line in a plain text file. A comma should separate the longitude and latitude in each line.
+            v-flex(lg6 xl4).pa-3
                 v-card(height="100%").elevation-5
                     v-card-title.blue-grey.lighten-4.elevation-3
-                        h2 <strong>Step 2</strong> Select Types of Geographic Entities
+                        h2 <strong>Step 2</strong> Select Input and Output Entity Types
                     v-card-text.pa-5
-                        p TODO: briefly about the options available
+                        p.body-2 &#9655; What type of geodata do you <strong>have</strong>?
+                        v-radio-group(v-model="inputType")
+                                    v-radio(value="zip" label="Zip Codes")
+                                    v-radio(value="address" label="Addresses")
+                                    v-radio(value="lon-lat" label="Geographic Coordinates")
+                        p.body-2 &#9655; What type(s) of geodata do you <strong>need</strong>?
                         v-select(
-                            label="Select"
                             :items="allGeoLevelsArr"
                             item-text="name"
                             item-value="code"
@@ -33,41 +43,59 @@ div.grey.lighten-3
                             chips
                             append-icon="map"
                             )
-                        br
-
-                        v-checkbox(label="Include geographic identification codes." v-model="includeGeoCodes").blue--text
-            v-flex(lg4).pa-3
+                        p.body-2 &#9655; Include geographic identification codes?
+                        v-switch(
+                            label="Yes, include codes."
+                            v-model="includeGeoCodes"
+                            ).blue--text
+            v-flex(lg6 xl4).pa-3
                 v-card(height="100%").elevation-5
                     v-card-title.indigo.lighten-4.elevation-3
                         h2 <strong>Step 3</strong> Upload File
                     v-card-text.pa-5
                         no-ssr
                             div
-                                div
-                                    v-progress-circular(:value="60")
-                                    upload-button(
-                                        title="Zip Codes"
-                                        :selectedCallback="fromZip")
-                                        .amber.lighten-1.ml-0
-                                    p Qui do sit sunt quis qui officia tempor labore sunt ullamco minim nostrud.
-                                div
-                                    upload-button(
-                                        title="Addresses / Landmarks"
-                                        :selectedCallback="fromAddress")
-                                        .light-green.lighten-1.ml-0
-                                    p Minim magna dolore incididunt duis reprehenderit incididunt est ad nostrud pariatur magna esse.
-                                div
-                                    upload-button(
-                                        title="Longitude-Latitude Pairs"
-                                        :selectedCallback="fromLonLat")
-                                        .indigo.lighten-3.ml-0
-                                    p Ad aliqua ex occaecat incididunt est reprehenderit dolor.
-    v-card.pa-5.ma-5
-        p <strong>Developed by</strong> <br> Asif Mehedi, Batten Institute for Entrepreneurship and Innovation at the University of Virginia Darden School of Business. Contact: MehediA@darden.virginia.edu.
-        p <strong>Acknowledgement</strong> <br> Data and APIs provided by the #[a(href="https://www.census.gov/") Census Bureau], the #[a(href="https://censusreporter.org/") Census Reporter], and #[a(href="https://www.bing.com/maps") Bing Maps].
-        p <strong>Citation and Use</strong> <br> You are free to use this software as you see fit. We request that you cite your use appropriately. Sample citations:
-        p APA (6th): Mehedi, A. (2018). CITee Geocoder (Version 1.0). Charlottesville: Cities Innovating Tomorrow's Entrepreneurial Ecosystems Project, Darden School of Business.
-        p Chicago: Mehedi, Asif. 2018. <em>Citee Geocoder</em> (version 1.0). Web. Charlottesville: Cities Innovating Tomorrow's Entrepreneurial Ecosystems Project, Darden School of Business.
+                                p Make sure you have selected the right input and output types. Click below to upload your file.
+                                upload-button(
+                                    title="Upload Your File"
+                                    :selectedCallback="uploadCallback"
+                                    )
+                                br
+                                v-progress-linear(
+                                    v-model="progressValue"
+                                    color="accent"
+                                    )
+                                h4.green--text.darken-2 {{ progressFeedback }}
+                                p.blue--text {{ filename }}
+    v-card.ma-5
+        v-layout(row wrap).ma-5.pa-5
+            v-flex(lg6 md12).px-3
+                p.body-2.mb-0 Developed by
+                p Asif Mehedi, Batten Institute for Entrepreneurship and Innovation at the University of Virginia Darden School of Business.
+                p.body-2.mb-0 Acknowledgment
+                p Data and APIs provided by the #[a(href="https://www.census.gov/") Census Bureau], the #[a(href="https://censusreporter.org/") Census Reporter], and #[a(href="https://www.bing.com/maps") Bing Maps].
+                p.body-2.mb-0 Citation and Use
+                p You are free to use this software as you see fit. We request that you cite your use appropriately. Sample citations:
+                p APA (6th): Mehedi, A. (2018). CITee Geocoder (Version 1.0). Charlottesville: Cities Innovating Tomorrow's Entrepreneurial Ecosystems Project, Darden School of Business.
+                p Chicago: Mehedi, Asif. 2018. <em>Citee Geocoder</em> (version 1.0). Web. Charlottesville: Cities Innovating Tomorrow's Entrepreneurial Ecosystems Project, Darden School of Business.
+                p.body-2.mb-0 Contact
+                p #[a(href="mehedia@darden.virginia.edu") mehedia@darden.virginia.edu]
+            v-flex(lg6 md12).px-3
+                p.body-2 FAQ
+                p <em>What problems does it solve?</em>
+                p Researchers dealing with spatial analyses often need to identify different geographic entities that contain a given location. The location data are usually in the form of addresses, Zip codes, or geographic coordinates. The desired outputs may be different legal and statistical geographic entities, such as county, metropolitan statistical area (MSA), public use microdata area (PUMA), congressional district, etc.
+
+                p This identification requires some effort, such as finding the right correspondence data (also known as crosswalks) and then performing a join operation between two datasets. This process may not yield satisfactory matches for a number of reasons. Many place names have variants, especially those involving abbreviated words. They may even be misspelled (common in survey data). The crosswalks data may not be complete.
+
+                p Here, one simply uploads the location data and indicates the output types. The app (actually, the underlying processes — see acknowledgment section) does the rest. Converting hundreds of location data usually does not take more than a few seconds.
+                p <em>What is CITee?</em>
+                p See here: #[a(href="http://citee.darden.virginia.edu/") http://citee.darden.virginia.edu/]
+
+                p <em>Will there be more input and output types in the future?</em>
+                p Quite possibly. But it's not a priority right now.
+
+                p <em> Does the app work on all browsers?</em>
+                p We recommend a recent version of Chrome. We have not yet tested this app on other browsers.
 </template>
 
 <script>
@@ -88,6 +116,9 @@ export default {
     components: { 'upload-button': UploadButton },
     data() {
         return {
+            inputType: [ 'zip',
+                'address',
+                'lon-lat' ],
             textFile: null,
             selectedGeoLevelsArr: [ {
                 code: '310',
@@ -95,6 +126,9 @@ export default {
             } ],
             allGeoLevelsArr,
             includeGeoCodes: true,
+            progressFeedback: '',
+            progressValue: 0,
+            filename: '',
         };
     },
     computed: {
@@ -103,6 +137,19 @@ export default {
         },
     },
     methods: {
+        uploadCallback( file ) {
+            this.progressFeedback = 'Getting data...';
+            if ( this.inputType === 'zip' ) {
+                this.fromZip( file );
+            } else if ( this.inputType === 'address' ) {
+                this.fromAddress( file );
+            } else if ( this.inputType === 'lon-lat' ) {
+                this.fromLonLat( file );
+            } else {
+            // TODO: Replace alert with dialog
+                alert( 'You must select an input type first.' );
+            }
+        },
         fromAddress( file ) {
             const vm = this;
             this.loadFile( file, loadedAddresses );
@@ -160,6 +207,7 @@ export default {
 
                 const promises = [];
                 const inputObjsArr = [];
+
                 lonLatsArr.forEach( lonLat => {
                     inputObjsArr.push( {
                         input: lonLat,
@@ -228,6 +276,7 @@ export default {
             reader.onload = fileLoadHandler;
         },
         writeCsv( dataArr, inputsArr, inputObjsArr ) {
+            this.progressFeedback = 'Writing data';
             const vm = this;
             const modDataArr = dataArr.reduce(
                 ( accum, curVal, curValIndex ) => {
@@ -261,8 +310,10 @@ export default {
 
             // Use date and time in file name
             const now = new Date();
-            const filename = `CITee-Geocoder_${ now.toLocaleString( 'en', { month: 'short' } ) }${ now.getDate() }-${ now.getFullYear() }__${ now.getHours() }h-${ now.getMinutes() }m-${ now.getSeconds() }s.csv`;
-            fs.saveAs( blob, filename );
+            this.filename = `CITee-Geocoder_${ now.toLocaleString( 'en', { month: 'short' } ) }${ now.getDate() }-${ now.getFullYear() }__${ now.getHours() }h-${ now.getMinutes() }m-${ now.getSeconds() }s.csv`;
+            fs.saveAs( blob, this.filename );
+            vm.progressValue = 100;
+            vm.progressFeedback = 'Task completed. Check data for accuracy. File saved as:';
         },
         getGeoDescFromCode( givenCode, arr ) {
             return arr.filter( el => el.code === givenCode )[ 0 ].name;
